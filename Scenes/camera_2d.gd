caef3002a94165cc63b83429031ab3ae68c5e913
דוד גולומב <golomb@bgu.ac.il>
2026-09-12 23:18:26 +0300

    restart

--- Scenes/camera_2d.gd
+++ Scenes/camera_2d.gd
@@ -3,11 +3,15 @@ extends Camera2D
 @onready var player: CharacterBody2D = $"../Player"
 
 @export var smooth_speed: float = 5.0
-
 var maxCemaraYPostion=0
+
+func die():
+	print("11")
+	get_tree().reload_current_scene()
 
 func calculatePosition(curent:float,target:float,delta:float) ->float:
 	var diff =(target-curent)
+	# movement Speed is about smooth speed * delta but allowed between 1 and 0
 	var movementSpeed= 1 - exp(-smooth_speed * delta)
 	return curent + diff * movementSpeed
 		
@@ -17,6 +21,9 @@ func _process(delta: float) -> void:
 	var a= calculatePosition(global_position.y, player.global_position.y, delta)
 	global_position.y= min(a,maxCemaraYPostion)
 	maxCemaraYPostion = global_position.y
+	print(global_position.y )
+	if global_position.y - player.global_position.y <-100:
+		die()
 	
 	
 	
