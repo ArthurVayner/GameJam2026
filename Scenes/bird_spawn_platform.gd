034516a90019ba2566df0c28bce2e2c1e8b67d74
דוד גולומב <golomb@bgu.ac.il>
2026-09-14 01:23:18 +0300

    export detection

---
+++ Scenes/bird_spawn_platform.gd
@@ -0,0 +1,3 @@
+extends Node2D
+
+@export_enum("Right:1", "Left:-1") var direction: int = 1
--- Scripts/bird_spawn_platform.gd
+++ Scripts/bird_spawn_platform.gd
@@ -1,10 +1,8 @@
 @tool
 extends "res://Scripts/Platform.gd"
 
-const BIRD = preload("res://Scenes/bird.tscn")
 const bird_scene  = preload("res://Scenes/bird.tscn")
 
-@export var direction: int =1
 
 @onready var detection_area: Area2D = $DetectionArea
 
@@ -13,9 +11,11 @@ const bird_scene  = preload("res://Scenes/bird.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	color = Color("D1EAEB")
+	var parent_direction = get_parent().direction
+	var new_bird = bird_scene.instantiate()
+	
 	onStand = func (body:Player) -> void:
-		var new_bird = bird_scene.instantiate()
-		new_bird.spanBirdAwayFromPlayer(body.position, 100 * direction, -38)
+		new_bird.spanBirdAwayFromPlayer(body.position, 100 * parent_direction, -38)
 		add_child(new_bird)
 		print("player is on my nest")
 	pass
