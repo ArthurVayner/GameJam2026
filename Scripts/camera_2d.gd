254a0e639d052f6ead96415d3ae3638356f4fed9
דוד גולומב <golomb@bgu.ac.il>
2026-09-13 10:26:59 +0300

    camera follows player in y axis

--- Scripts/camera_2d.gd
+++ Scripts/camera_2d.gd
@@ -3,7 +3,6 @@ extends Camera2D
 @onready var player: CharacterBody2D = $"../Player"
 
 @export var smooth_speed: float = 5.0
-var maxCemaraYPostion=0
 
 func die():
 	print("11")
@@ -18,12 +17,7 @@ func calculatePosition(curent:float,target:float,delta:float) ->float:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	global_position.x= calculatePosition(global_position.x, player.global_position.x, delta)
-	var a= calculatePosition(global_position.y, player.global_position.y, delta)
-	global_position.y= min(a,maxCemaraYPostion)
-	maxCemaraYPostion = global_position.y
-	#print(global_position.y )
-	if global_position.y - player.global_position.y <-100:
-		die()
+	global_position.y= calculatePosition(global_position.y, player.global_position.y, delta)
 	
 	
 	
