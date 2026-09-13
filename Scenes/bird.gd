5fa8a7fc7cbb0769bc4bcd1c0b69a7d150beea88
דוד גולומב <golomb@bgu.ac.il>
2026-09-13 15:34:46 +0300

    add bird

--- Scenes/bird.gd
+++ Scenes/bird.gd
@@ -4,6 +4,7 @@ var speed = 100.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print("bird booste")
 	pass # Replace with function body.
 
 func _physics_process(delta: float) -> void:
