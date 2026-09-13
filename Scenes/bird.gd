c3617a1f1e9e476fd9aee43fc5a671b0f12d0dd6
Artur Vainer <chahla8m@gmail.com>
2026-09-13 17:55:55 +0300

    Just added folder

--- Scenes/bird.gd
+++ Scenes/bird.gd
@@ -13,7 +13,7 @@ func _physics_process(delta: float) -> void:
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func _on_body_entered(body: Node2D) -> void:
