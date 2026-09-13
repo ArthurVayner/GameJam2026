c0cb9969a6b43ad4b6984dcd267d63f862582c0c
דוד גולומב <golomb@bgu.ac.il>
2026-09-13 17:29:23 +0300

    change reset_bird_timer

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -12,7 +12,7 @@ var falling_object_offset: int = 200
 var falling_object_interval: int = 2
 
 func reset_bird_timer() -> void:
-	bird_timer.start(randf_range(1,3))
+	bird_timer.start(randf_range(2,7))
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -45,7 +45,7 @@ func spawneBird() -> void:
 
 func _on_bird_timer_timeout() -> void:
 	var birdsAmount : int= [1,1,1,2,2,3].pick_random()
-	for i in range(0,birdsAmount): 
+	for i in range(0, birdsAmount): 
 		spawneBird()
 	reset_bird_timer()
 	
