4ed2f369bd4c4cd8c7a148a2f837c9894cc0ca73
דוד גולומב <golomb@bgu.ac.il>
2026-09-14 01:30:59 +0300

    fix ininstantiate bug

--- Scripts/bird_spawn_platform.gd
+++ Scripts/bird_spawn_platform.gd
@@ -12,14 +12,16 @@ const bird_scene  = preload("res://Scenes/bird.tscn")
 func _ready() -> void:
 	color = Color("D1EAEB")
 	var parent_direction = get_parent().direction
-	var new_bird = bird_scene.instantiate()
 	
+	super._ready()	
 	onStand = func (body:Player) -> void:
+		var new_bird = bird_scene.instantiate()
+			
 		new_bird.spanBirdAwayFromPlayer(body.position, 100 * parent_direction, -38)
 		add_child(new_bird)
 		print("player is on my nest")
 	pass
-	super._ready()
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
