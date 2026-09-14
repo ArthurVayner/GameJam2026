ed796f0410986c51f19bd0c2c28c843a8794c8f4
Artur Vainer <chahla8m@gmail.com>
2026-09-14 16:40:04 +0300

    Implemented vine and lead textures

--- Scripts/Platform.gd
+++ Scripts/Platform.gd
@@ -1,17 +1,22 @@
 @tool
 extends StaticBody2D
 
+
+enum PLATFROM_TEXTURE {ONE, ONE_ROTATED, TWO, TWO_ROTATED, THREE, THREE_ROTATED, FOUR, FOUR_ROTATED, INVISIBLE}
+@export var platform_texture: PLATFROM_TEXTURE
+
+
 @export var color: Color = Color("00fa9a"):
 	set(value):
 		color = value
 
-@export var width: float =100.0:
+@export var width: float =24.0:
 	set(value):
 		width=value
 		if is_node_ready():
 			setSize()
 #		
-@export var height: float = 20.0:
+@export var height: float = 8.0:
 	set(value):
 		height=value
 		if is_node_ready():
@@ -19,6 +24,8 @@ extends StaticBody2D
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var detection_area_shape: CollisionShape2D = $DetectionArea/CollisionShape2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
 
 var onStand = func (body:Player) -> void:
 	print("player is on me")
@@ -27,6 +34,7 @@ var onStand = func (body:Player) -> void:
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	setSize()
+	set_texture()
 	
 	
 func setSize()-> void:
@@ -40,6 +48,31 @@ func setSize()-> void:
 		detection_area_shape.shape.size = Vector2(width - 1, 2 )
 		detection_area_shape.position = Vector2(0, -height/2 )
 		
+
+func set_texture() -> void:
+	match platform_texture:
+		PLATFROM_TEXTURE.ONE:
+			sprite_2d.texture = load("res://Assets/images/Platform.png")
+		PLATFROM_TEXTURE.ONE_ROTATED:
+			sprite_2d.texture = load("res://Assets/images/Platform.png")
+			scale.x = -1
+		PLATFROM_TEXTURE.TWO:
+			sprite_2d.texture = load("res://Assets/images/Platform2.png")
+		PLATFROM_TEXTURE.TWO_ROTATED:
+			sprite_2d.texture = load("res://Assets/images/Platform2.png")
+			scale.x = -1
+		PLATFROM_TEXTURE.THREE:
+			sprite_2d.texture = load("res://Assets/images/Platform3.png")
+		PLATFROM_TEXTURE.THREE_ROTATED:
+			sprite_2d.texture = load("res://Assets/images/Platform3.png")
+			scale.x = -1
+		PLATFROM_TEXTURE.FOUR:
+			sprite_2d.texture = load("res://Assets/images/Platform4.png")
+		PLATFROM_TEXTURE.FOUR_ROTATED:
+			sprite_2d.texture = load("res://Assets/images/Platform4.png")
+			scale.x = -1
+		9:
+			sprite_2d.texture = null
 
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
