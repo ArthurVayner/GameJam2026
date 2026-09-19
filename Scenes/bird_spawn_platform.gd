17f578da164b103428e8ceab12439830879a9cbd
yaniv <yaniv.gol.h@gmail.com>
2026-09-19 22:51:56 +0300

    change main level

--- Scenes/bird_spawn_platform.gd
+++ Scenes/bird_spawn_platform.gd
@@ -1,3 +1,4 @@
 extends Node2D
 
 @export_enum("Right:1", "Left:-1") var direction: int = 1
+@export var bird_distance_from_platform: float = 100.0
--- Scripts/bird_spawn_platform.gd
+++ Scripts/bird_spawn_platform.gd
@@ -12,13 +12,14 @@ const bird_scene  = preload("res://Scenes/bird.tscn")
 func _ready() -> void:
 	color = Color("D1EAEB")
 	var parent_direction = get_parent().direction
+	var parent_bird_distance_from_platform = get_parent().bird_distance_from_platform
 	
 	super._ready()	
 	onStand = func (body:Player) -> void:
 		var new_bird = bird_scene.instantiate()
 			
 		add_child(new_bird)
-		new_bird.spanBirdAwayFromPlayer(body.global_position, 100 * parent_direction, -5.0)
+		new_bird.spanBirdAwayFromPlayer(body.global_position, parent_bird_distance_from_platform * parent_direction, -5.0)
 		print("player is on my nest")
 	pass
 
