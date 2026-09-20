ac27fe232d533a77167c3d7a4f493dabc4bf5b98
Artur Vainer <chahla8m@gmail.com>
2026-09-20 19:23:30 +0300

    yes

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -6,10 +6,6 @@ const bird_scene  = preload("res://Scenes/bird.tscn")
 @onready var falling_obj_timer: Timer = $FallingObjTimer
 @onready var bird_timer: Timer = $BirdTimer
 @onready var player: Player = $Player
-@onready var rich_text_label: RichTextLabel = $RichTextLabel
-
-
-
 
 
 var falling_object_offset: int = 200
