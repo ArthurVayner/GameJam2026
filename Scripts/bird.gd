0e9457687229caf7b956bc4bbca629e168b005e0
yaniv <yaniv.gol.h@gmail.com>
2026-09-20 18:54:05 +0300

    change knock back

--- Scripts/bird.gd
+++ Scripts/bird.gd
@@ -31,7 +31,7 @@ func _physics_process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		body.give_knockback(Vector2(200 * direction, -100))
+		body.give_knockback(Vector2(250 * direction, -100))
 		queue_free()
 
 func _on_falpping_sound_timer_timeout() -> void:
