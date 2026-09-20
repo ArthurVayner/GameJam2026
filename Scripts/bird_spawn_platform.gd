58dfa4cade2f17855026e9a9b62624fa280fe892
yaniv <yaniv.gol.h@gmail.com>
2026-09-20 16:47:50 +0300

    fix onStandFunc

--- Scripts/bird_spawn_platform.gd
+++ Scripts/bird_spawn_platform.gd
@@ -7,20 +7,23 @@ const bird_scene  = preload("res://Scenes/bird.tscn")
 @onready var detection_area: Area2D = $DetectionArea
 
 
+func onStandFunc(body:Player) -> void:
+	var parent_direction = get_parent().direction
+	var parent_bird_distance_from_platform = get_parent().bird_distance_from_platform
+	var new_bird = bird_scene.instantiate()
+		
+	add_child(new_bird)
+	new_bird.spanBirdAwayFromPlayer(body.global_position, parent_bird_distance_from_platform * parent_direction, -5.0)
+	new_bird.set_direction()
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	color = Color("D1EAEB")
-	var parent_direction = get_parent().direction
-	var parent_bird_distance_from_platform = get_parent().bird_distance_from_platform
 	
 	super._ready()	
 	onStand = func (body:Player) -> void:
-		var new_bird = bird_scene.instantiate()
-		
-		add_child(new_bird)
-		new_bird.spanBirdAwayFromPlayer(body.global_position, parent_bird_distance_from_platform * parent_direction, -5.0)
-		new_bird.set_direction()
+		call_deferred("onStandFunc", body)
 		print("player is on my nest")
 	pass
 
